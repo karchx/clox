e9f6c5735e11d54e56f1a0ae8c261390ed7cea20
Стив <kenethra.74@gmail.com>
2022-10-14 21:39:52 -0600

    feat: valida errors compile

--- main.c
+++ main.c
@@ -25,12 +25,26 @@ static void repl() {
 static char *readFile(const char *path) {
   FILE *file = fopen(path, "rb");
 
+  if (file == NULL) {
+    fprintf(stderr, "Could not open file \"%s\".\n", path);
+    exit(74);
+  }
+
   fseek(file, 0L, SEEK_END);
   size_t fileSize = ftell(file);
   rewind(file);
 
   char *buffer = (char *)malloc(fileSize + 1);
+  if (buffer == NULL) {
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+    exit(74);
+  }
+
   size_t byteRead = fread(buffer, sizeof(char), fileSize, file);
+  if (byteRead < fileSize) {
+    fprintf(stderr, "Could not read file \"%s\".\n",path);
+    exit(74);
+  }
   buffer[byteRead] = '\0';
 
   fclose(file);
