e47c9660f777b520dca4303f871149d0844390cc
Стив <kenethra.74@gmail.com>
2022-10-23 14:19:27 -0600

    feat: local variables

--- compiler.c
+++ compiler.c
@@ -39,7 +39,19 @@ typedef struct {
   ParseFn precedence;
 } ParseRule;
 
+typedef struct {
+  Token name;
+  int depth;
+} Local;
+
+typedef struct {
+  Local locals[UINT8_COUNT];
+  int localCount;
+  int scopeDepth;
+} Compiler;
+
 Parser parser;
+Compiler *current = NULL;
 Chunk *compilingChunk;
 
 static Chunk *currentChunk() { return compilingChunk; }
@@ -120,6 +132,12 @@ static uint8_t makeConstant(Value value) {
 
 static void emitConstant(Value value) {
   emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void initCompler(Compiler *compiler) {
+  compiler->localCount = 0;
+  compiler->scopeDepth = 0;
+  current = compiler;
 }
 
 static void endCompiler() {
@@ -392,6 +410,8 @@ static void statement() {
 
 bool compile(const char *source, Chunk *chunk) {
   initScanner(source);
+  Compiler compiler;
+  initCompler(&compiler);
   compilingChunk = chunk;
 
   parser.hadError = false;
