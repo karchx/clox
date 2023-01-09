f2e5294b304008ace2900c4aa03168422b0529e1
Стив <kenethra.74@gmail.com>
2023-01-08 18:25:51 -0600

    feat: add function in struct compiler

--- compiler.c
+++ compiler.c
@@ -45,7 +45,15 @@ typedef struct {
   int depth;
 } Local;
 
+typedef enum {
+  TYPE_FUNCTION,
+  TYPE_SCRIPT
+} FunctionType;
+
 typedef struct {
+  ObjFunction *function;
+  FunctionType type;
+
   Local locals[UINT8_COUNT];
   int localCount;
   int scopeDepth;
