a2fabe8fb70902c05f7c5f55f1438c08d1a9a902
Стив <kenethra.74@gmail.com>
2023-01-08 18:34:58 -0600

    feat: compiler return objFunction

--- compiler.c
+++ compiler.c
@@ -5,6 +5,7 @@
 #include "chunk.h"
 #include "common.h"
 #include "compiler.h"
+#include "object.h"
 #include "scanner.h"
 
 #ifdef DEBUG_PRINT_CODE
@@ -170,19 +171,29 @@ static void patchJump(int offset) {
   currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompler(Compiler *compiler) {
+static void initCompler(Compiler *compiler, FunctionType type) {
+  compiler->function = NULL;
+  compiler->type = type;
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
+  compiler->function = newFunction();
   current = compiler;
+
+  Local *local = &current->locals[current->localCount++];
+  local->depth = 0;
+  local->name.start = "";
+  local->name.length = 0;
 }
 
-static void endCompiler() {
+static ObjFunction *endCompiler() {
   emitReturn();
+  ObjFunction *function = current->function;
 #ifdef DEBUG_PRINT_CODE
   if (!parser.hadError) {
     disassembleChunk(currentChunk(), "code");
   }
 #endif
+  return function;
 }
 
 static void beginScope() { current->scopeDepth++; }
@@ -647,8 +658,7 @@ static void statement() {
 bool compile(const char *source, Chunk *chunk) {
   initScanner(source);
   Compiler compiler;
-  initCompler(&compiler);
-  compilingChunk = chunk;
+  initCompler(&compiler, TYPE_SCRIPT);
 
   parser.hadError = false;
   parser.panicMode = false;
